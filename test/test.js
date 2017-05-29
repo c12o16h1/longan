451bf7310fbbb14483dabe809e71901d58a61a8b
Yurii Kovalenko <ua.preston@gmail.com>
2017-05-29 18:50:31 +0700

    code structuring

--- test/test.js
+++ test/test.js
@@ -2,14 +2,23 @@ import LonganComponent from '../lib/component'
 const MainComponent = new LonganComponent('MainComponent');
 const HeaderComponent = new LonganComponent('HeaderComponent');
 HeaderComponent.asyncAdded = 0;
-MainComponent.run = function () {
-    MainComponent.use(HeaderComponent);
 
-    window.MainComponent = MainComponent;
-    window.HeaderComponent = HeaderComponent;
+HeaderComponent.nestedObject = {
+    name: "HeaderComponenent nested object",
+    changeMe: 5
 };
+
+MainComponent.run = function () {
+    this.use(HeaderComponent);
+    let iNeedThis = this.use(HeaderComponent.nestedObject);
+};
+
 MainComponent.run();
 
 setTimeout(function () {
     HeaderComponent.asyncAdded = 10;
 }, 1000);
+
+setTimeout(function () {
+    HeaderComponent.nestedObject.changeMe = 10;
+}, 2000);
